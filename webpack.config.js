84d0814d0abc769ed90a10a185cd22b0d7526458
Michael Jungo <michaeljungo92@gmail.com>
2017-03-06 23:35:52 +0100

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: './dist',
+    path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.[hash:6].js'
   },
   plugins: [
