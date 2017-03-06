08390ac346b446536de07f9f8c8e61d327b97f24
Michael Jungo <michaeljungo92@gmail.com>
2017-03-06 23:55:38 +0100

    Mention public path in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,10 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.[hash:6].js'
+    filename: 'app.bundle.[hash:6].js',
+    // Public path affects URL of the assets
+    // This would make it <script src="/public/app.bundle.aa9cfd.js"></script>
+    // publicPath: '/public/'
   },
   plugins: [
     new HtmlWebpackPlugin({
